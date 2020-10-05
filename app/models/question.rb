27bcfe296579f6429cce501a7144a7b4d08ea691
barchuk oleg <barchukoleg12@gmail.com>
2020-10-05 19:33:17 +0300

    validation corrected

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,5 @@ class Question < ApplicationRecord
 
   belongs_to :user
 
-  validates :text, :user, presence: true
-  validates :text, length: { maximum: 255 }
+  validates :text, presence: true, length: { maximum: 255 }
 end
