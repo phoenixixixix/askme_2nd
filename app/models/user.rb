384956a2ba5f77515debef92ca1d9255110ff1fe
barchuk oleg <barchukoleg12@gmail.com>
2020-10-05 19:23:25 +0300

    code style corrected

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,37 +4,24 @@ class User < ApplicationRecord
   # параметри роботи модуля шифрування паролів
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
+  EMAIL_FORMAT = /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  PASSWORD_FORMAT = /\A[a-z\d_]*\z/i
 
   has_many :questions
 
-  validates :email, :username, presence: true, uniqueness: true
-  validates :email, format: { with: /\A[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
-  validates :username, length: { maximum: 40 }, format: { with: /\A[a-z\d_]*\z/i }
-
   attr_accessor :password
+
+
+  validates :email, :username, presence: true, uniqueness: true
+  validates :email, format: { with: EMAIL_FORMAT }
+  validates :username, length: { maximum: 40 }, format: { with: PASSWORD_FORMAT }
 
   validates :password, presence: true, on: :create
   validates :password, confirmation: true
 
-  # старіший вигляд
-  # validates_presence_of :password, on: :create
-  # validates_confirmation_of :password
-
+  
   before_save :encrypt_password
 
-  def encrypt_password
-    if self.password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
-      )
-    end
-  end
-
-  def self.hash_to_string(password_hash)
-    password_hash.unpack('H*')[0]
-  end
 
   def self.authenticate(email, password)
     user = find_by(email: email)
@@ -43,6 +30,20 @@ class User < ApplicationRecord
       user
     else
       nil
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
     end
   end
 end
